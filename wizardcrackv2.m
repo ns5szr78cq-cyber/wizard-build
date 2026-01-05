821e85a895d2fdce28395b5227f7efe055a5ecc8
ns5szr78cq-cyber <ns5szr78cq@privaterelay.appleid.com>
2026-01-05 11:59:43 +0000

    mlml

--- wizardcrackv2.m
+++ wizardcrackv2.m
@@ -2,67 +2,65 @@
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 
-// إجابات إجبارية لأي سؤال من الحماية
+// دوال لتثبيت التاريخ والنجاح
 static _Bool returnYes() { return YES; }
-static id returnKey() { return @"WeekivEnZldeIn4pRd9YJ5nCbUOclX17"; }
+static id fixedDate() { return [NSDate dateWithTimeIntervalSince1970:1893456000]; } // تاريخ في 2030
 
 __attribute__((constructor))
 static void init() {
-    // 1. منع الكراش فوراً
+    // 1. منع الكراش الناتج عن اختلاف الوقت أو اكتشاف التلاعب
     Method exitM = class_getInstanceMethod([UIApplication class], NSSelectorFromString(@"terminateWithSuccess"));
     if (exitM) method_setImplementation(exitM, imp_implementationWithBlock(^{}));
 
-    // 2. هجوم متكرر على الذاكرة لضمان صيد ملف الـ 80 ميجا (Core)
-    for (float d = 1.0; d <= 7.0; d += 2.0) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(d * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            int numClasses = objc_getClassList(NULL, 0);
-            if (numClasses > 0) {
-                Class *classes = (Class *)malloc(sizeof(Class) * numClasses);
-                numClasses = objc_getClassList(classes, numClasses);
-                for (int i = 0; i < numClasses; i++) {
-                    NSString *cName = NSStringFromClass(classes[i]);
-                    // استهداف كل كلاسات Wizard و Core
-                    if ([cName containsString:@"Wizard"] || [cName containsString:@"Core"] || [cName containsString:@"Sentry"]) {
-                        unsigned int mCount;
-                        Method *methods = class_copyMethodList(classes[i], &mCount);
-                        for (unsigned int j = 0; j < mCount; j++) {
-                            SEL sel = method_getName(methods[j]);
-                            NSString *mName = NSStringFromSelector(sel);
-                            // كسر دوال التحقق بجميع أنواعها
-                            if ([mName hasPrefix:@"is"] || [mName containsString:@"Valid"] || [mName containsString:@"Auth"]) {
-                                class_replaceMethod(classes[i], sel, (IMP)returnYes, "B@:");
-                            }
-                            // حقن كود تفعيل افتراضي في حال طلبه
-                            if ([mName containsString:@"Key"] || [mName containsString:@"Code"]) {
-                                class_replaceMethod(classes[i], sel, (IMP)returnKey, "@@:");
-                            }
+    // 2. تزييف التاريخ والوقت داخل ذاكرة المنيو (Wizard)
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        int numClasses = objc_getClassList(NULL, 0);
+        if (numClasses > 0) {
+            Class *classes = (Class *)malloc(sizeof(Class) * numClasses);
+            numClasses = objc_getClassList(classes, numClasses);
+            for (int i = 0; i < numClasses; i++) {
+                NSString *cName = NSStringFromClass(classes[i]);
+                if ([cName containsString:@"Wizard"] || [cName containsString:@"Auth"]) {
+                    unsigned int mCount;
+                    Method *methods = class_copyMethodList(classes[i], &mCount);
+                    for (unsigned int j = 0; j < mCount; j++) {
+                        NSString *mName = NSStringFromSelector(method_getName(methods[j]));
+                        // تثبيت تاريخ الانتهاء (Expiry Date)
+                        if ([mName containsString:@"Date"] || [mName containsString:@"Time"]) {
+                            class_replaceMethod(classes[i], method_getName(methods[j]), (IMP)fixedDate, "@@:");
                         }
-                        free(methods);
+                        // تخطي فحص الصلاحية
+                        if ([mName hasPrefix:@"is"] || [mName containsString:@"Valid"]) {
+                            class_replaceMethod(classes[i], method_getName(methods[j]), (IMP)returnYes, "B@:");
+                        }
                     }
+                    free(methods);
                 }
-                free(classes);
             }
-        });
-    }
+            free(classes);
+        }
+    });
 
-    // 3. رسالة DooN UP النهائية وإجبار المنيو على الظهور
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    // 3. حذف واجهة التحقق فور ظهورها
+    NSTimer *uiTimer = [NSTimer scheduledTimerWithTimeInterval:0.3 repeats:YES block:^(NSTimer * _Nonnull timer) {
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN UP" 
-                                       message:@"تم التفعيل بنجاح ✅\nالمنيو جاهز الآن" 
-                                       preferredStyle:UIAlertControllerStyleAlert];
+        UIViewController *root = window.rootViewController;
         
-        [alert addAction:[UIAlertAction actionWithTitle:@"استمرار" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-            // محاولة إظهار أي واجهة مخفية تابعة للمنيو
-            for (UIView *v in window.subviews) {
-                v.hidden = NO; 
-                v.alpha = 1.0;
-                if ([NSStringFromClass([v class]) containsString:@"Wizard"]) [window bringSubviewToFront:v];
-            }
-            // إرسال إشارات برمجية لإظهار المنيو
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"WizardShowMenu" object:nil];
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"ShowMenu" object:nil];
-        }]];
-        [window.rootViewController presentViewController:alert animated:YES completion:nil];
+        // إذا ظهرت أي شاشة تحتوي على كلمة Wizard أو Auth يتم إخفاؤها فوراً
+        if ([NSStringFromClass([root class]) containsString:@"Wizard"] || [NSStringFromClass([root class]) containsString:@"Auth"]) {
+            root.view.hidden = YES;
+            [root.view removeFromSuperview];
+        }
+    }];
+    [[NSRunLoop mainRunLoop] addTimer:uiTimer forMode:NSDefaultRunLoopMode];
+
+    // 4. رسالة DooN UP النهائية
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *win = [UIApplication sharedApplication].keyWindow;
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN UP" 
+                                       message:@"تم التفعيل وتثبيت التاريخ ✅\nجاهز للاستخدام" 
+                                       preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"دخول" style:UIAlertActionStyleDefault handler:nil]];
+        [win.rootViewController presentViewController:alert animated:YES completion:nil];
     });
 }
