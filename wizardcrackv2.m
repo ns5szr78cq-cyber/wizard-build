236046e42e24459f666ee8955e6a1db73a3eb411
ns5szr78cq-cyber <ns5szr78cq@privaterelay.appleid.com>
2026-01-05 09:24:56 +0000

    dddddd

--- wizardcrackv2.m
+++ wizardcrackv2.m
@@ -3,17 +3,18 @@
 #import <UIKit/UIKit.h>
 
 @implementation NSObject (WizardHook)
-// دالة موحدة لترد بـ "نعم" على أي طلب تحقق من الهوية أو الكود
-- (_Bool)hooked_universal_true { return YES; }
+// دالة لرد بـ "نعم"
+- (_Bool)hooked_true { return YES; }
+// دالة لرد برمز النجاح من السيرفر (200 تعني تم القبول)
+- (NSInteger)hooked_status { return 200; }
 @end
 
 static void ShowAlert() {
-    // إظهار الرسالة بعد 3 ثواني من فتح اللعبة
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
         if (root) {
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN UP ✅" 
-                                           message:@"تم تفعيل التخطي بنجاح\nاكتب أي كود وسيتم الدخول" 
+                                           message:@"تم تفعيل التخطي الشامل وتزييف رد السيرفر\nاكتب أي كود وجرب الآن" 
                                            preferredStyle:UIAlertControllerStyleAlert];
             [alert addAction:[UIAlertAction actionWithTitle:@"استمرار" style:UIAlertActionStyleDefault handler:nil]];
             [root presentViewController:alert animated:YES completion:nil];
@@ -24,26 +25,29 @@ static void ShowAlert() {
 __attribute__((constructor))
 static void init() {
     ShowAlert();
-    
-    // قائمة الوظائف البرمجية المسؤولة عن التحقق في أغلب أنواع الحماية
-    NSArray *methodsToHook = @[
-        @"_checkAuthentication",
-        @"isAuthorized",
-        @"isAuthenticated",
-        @"checkPassword:",
-        @"isValid",
-        @"isActivated"
-    ];
 
-    // استهداف مكتبة التوصيل الخاصة باللعبة
-    Class c = NSClassFromString(@"GCDWebServerConnection");
-    if (c) {
-        for (NSString *methodName in methodsToHook) {
-            Method orig = class_getInstanceMethod(c, NSSelectorFromString(methodName));
-            Method hook = class_getInstanceMethod([NSObject class], @selector(hooked_universal_true));
-            if (orig && hook) {
-                method_setImplementation(orig, method_getImplementation(hook));
+    // 1. استهداف كلاسات الاتصال وتخطي التحقق الداخلي
+    NSArray *classes = @[@"GCDWebServerConnection", @"GCDWebServer"];
+    NSArray *methods = @[@"_checkAuthentication", @"isAuthorized", @"isAuthenticated", @"isValid"];
+    
+    for (NSString *className in classes) {
+        Class c = NSClassFromString(className);
+        if (c) {
+            for (NSString *m in methods) {
+                Method orig = class_getInstanceMethod(c, NSSelectorFromString(m));
+                if (orig) {
+                    method_setImplementation(orig, method_getImplementation(class_getInstanceMethod([NSObject class], @selector(hooked_true))));
+                }
             }
+        }
+    }
+
+    // 2. تزييف رد السيرفر ليكون دائماً "ناجح" (Status 200)
+    Class responseClass = NSClassFromString(@"GCDWebServerResponse");
+    if (responseClass) {
+        Method origStatus = class_getInstanceMethod(responseClass, NSSelectorFromString(@"statusCode"));
+        if (origStatus) {
+            method_setImplementation(origStatus, method_getImplementation(class_getInstanceMethod([NSObject class], @selector(hooked_status))));
         }
     }
 }
