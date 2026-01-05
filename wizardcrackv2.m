1a4ba44d4a21d73cca60aff2dee02ace4bd86789
ns5szr78cq-cyber <ns5szr78cq@privaterelay.appleid.com>
2026-01-05 08:41:34 +0000

    DooN

--- wizardcrackv2.m
+++ wizardcrackv2.m
@@ -3,36 +3,31 @@
 #import <UIKit/UIKit.h>
 
 @implementation NSObject (WizardHook)
-- (_Bool)hooked_checkAuthentication { return YES; }
-- (_Bool)hooked_isAuthorized { return YES; }
+- (_Bool)hooked_auth { return YES; }
 @end
 
 static void ShowAlert() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Wizard Hook" 
-                                                                       message:@"تم تفعيل التخطّي بنجاح! جرب الدخول الآن" 
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"تم" style:UIAlertActionStyleDefault handler:nil]];
-        [root presentViewController:alert animated:YES completion:nil];
+        if (root) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Wizard Status" 
+                                           message:@"تم تفعيل التخطي بنجاح ✅" 
+                                           preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+            [root presentViewController:alert animated:YES completion:nil];
+        }
     });
 }
 
 __attribute__((constructor))
 static void init() {
-    // إظهار الرسالة داخل اللعبة بعد 3 ثواني
     ShowAlert();
-
-    // تخطي الحماية
-    NSArray *classes = @[@"GCDWebServerConnection", @"GCDWebServer"];
-    for (NSString *className in classes) {
-        Class c = NSClassFromString(className);
-        if (c) {
-            Method orig = class_getInstanceMethod(c, NSSelectorFromString(@"_checkAuthentication"));
-            if (orig) {
-                Method hook = class_getInstanceMethod([NSObject class], @selector(hooked_checkAuthentication));
-                method_setImplementation(orig, method_getImplementation(hook));
-            }
+    Class c = NSClassFromString(@"GCDWebServerConnection");
+    if (c) {
+        Method orig = class_getInstanceMethod(c, NSSelectorFromString(@"_checkAuthentication"));
+        Method hook = class_getInstanceMethod([NSObject class], @selector(hooked_auth));
+        if (orig && hook) {
+            method_setImplementation(orig, method_getImplementation(hook));
         }
     }
 }
