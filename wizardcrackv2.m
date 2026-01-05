e96df6d9f1985b50156f8f130d414bfd2b2f08f9
ns5szr78cq-cyber <ns5szr78cq@privaterelay.appleid.com>
2026-01-05 10:57:44 +0000

    vvvv

--- wizardcrackv2.m
+++ wizardcrackv2.m
@@ -2,26 +2,24 @@
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 
-// كود الدخول الخاص بك
-#define MY_DOON_KEY @"WeekivEnZldeIn4pRd9YJ5nCbUOclX17"
-
-@implementation NSObject (DoonGuard)
+@implementation NSObject (DoonFix)
+// 1. إجبار حالة التفعيل على "نعم"
 - (_Bool)doon_true { return YES; }
-- (NSInteger)doon_200 { return 200; }
-- (NSData *)doon_data {
-    NSDictionary *dict = @{@"status":@"success", @"valid":@YES, @"license":MY_DOON_KEY};
-    return [NSJSONSerialization dataWithJSONObject:dict options:0 error:nil];
-}
+
+// 2. إعطاء تاريخ انتهاء طويل جداً لمنع رسالة "انتهى الصلاحية" والكراش
+- (NSString *)doon_date { return @"2030-01-01"; }
+
+// 3. إعطاء رقم أيام كبير
+- (int)doon_days { return 9999; }
 @end
 
-// إظهار رسالة DooN UP ✅
 static void ShowDoonAlert() {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
         UIViewController *root = window.rootViewController;
         if (root) {
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN UP ✅" 
-                                           message:@"تم إيقاف مؤقت الإغلاق\nتعديل الكود نشط الآن" 
+                                           message:@"تم تفعيل حماية البيانات\nالتاريخ محقن حتى 2030\nاكتب أي كود الآن" 
                                            preferredStyle:UIAlertControllerStyleAlert];
             [alert addAction:[UIAlertAction actionWithTitle:@"استمرار" style:UIAlertActionStyleDefault handler:nil]];
             [root presentViewController:alert animated:YES completion:nil];
@@ -31,33 +29,29 @@ static void ShowDoonAlert() {
 
 __attribute__((constructor))
 static void init() {
-    // 1. التنفيذ الفوري (خلال 0.5 ثانية) لسباق عداد الإغلاق
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    // إيقاف دالة "إغلاق التطبيق" الإجبارية من السيستم
+    Method terminate = class_getInstanceMethod([UIApplication class], NSSelectorFromString(@"terminateWithSuccess"));
+    if (terminate) method_setImplementation(terminate, imp_implementationWithBlock(^{ 
+        NSLog(@"DooN UP: Blocking Exit..."); 
+    }));
+
+    // تنفيذ الحقن بعد ثانية واحدة لضمان ثبات اللعبة
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        // أ- تعطيل أي أمر "خروج" (Exit) ترسل اللعبة لنفسها
-        Method exitMethod = class_getInstanceMethod([UIApplication class], NSSelectorFromString(@"terminateWithSuccess"));
-        if (exitMethod) {
-            method_setImplementation(exitMethod, imp_implementationWithBlock(^{ 
-                NSLog(@"DooN UP: Blocking Auto-Exit..."); 
-            }));
-        }
-
-        // ب- تزييف بيانات السيرفر (رد النجاح)
-        Class respClass = NSClassFromString(@"GCDWebServerResponse");
-        if (respClass) {
-            method_setImplementation(class_getInstanceMethod(respClass, @selector(statusCode)), method_getImplementation(class_getInstanceMethod([NSObject class], @selector(doon_200))));
-            method_setImplementation(class_getInstanceMethod(respClass, @selector(data)), method_getImplementation(class_getInstanceMethod([NSObject class], @selector(doon_data))));
-        }
-
-        // ج- تفعيل كلاسات الحماية
-        NSArray *classes = @[@"LicenseManager", @"GCDWebServerConnection", @"AuthService"];
+        NSArray *classes = @[@"LicenseManager", @"AuthService", @"AppConfig", @"GCDWebServerConnection"];
+        
         for (NSString *cName in classes) {
             Class c = NSClassFromString(cName);
             if (c) {
+                // تخطي الفحص المنطقي (هل الكود صح؟)
                 SEL s1 = NSSelectorFromString(@"isActivated");
-                SEL s2 = NSSelectorFromString(@"_checkAuthentication");
+                SEL s2 = NSSelectorFromString(@"isValid");
                 if (class_getInstanceMethod(c, s1)) class_replaceMethod(c, s1, method_getImplementation(class_getInstanceMethod([NSObject class], @selector(doon_true))), "B@:");
                 if (class_getInstanceMethod(c, s2)) class_replaceMethod(c, s2, method_getImplementation(class_getInstanceMethod([NSObject class], @selector(doon_true))), "B@:");
+
+                // حقن التاريخ (عشان يظهر جوه الهاك وما يكراشش)
+                SEL sDate = NSSelectorFromString(@"getExpiryDate");
+                if (class_getInstanceMethod(c, sDate)) class_replaceMethod(c, sDate, method_getImplementation(class_getInstanceMethod([NSObject class], @selector(doon_date))), "@@:");
             }
         }
         
